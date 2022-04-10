b15c57b99b580d06badbb7d4d712de9c0bf0ae76
mdsaharshital <mdsaharshitol@gmail.com>
2022-04-10 17:36:44 +0600

    signup setup and error for not matched password

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,25 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Signup.css";
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleEmailBlur = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordBlur = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleConfirmPasswordBlur = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+  const handleCreateUSer = (e) => {
+    e.preventDefault();
+    setError("");
+    if (password !== confirmPassword) {
+      setError(`Password didn't matched.`);
+      return;
+    }
+  };
+
   return (
     <div className="form-container">
       <div>
         <h2 className="form-title">Sign up</h2>
-        <form>
+        <form onSubmit={handleCreateUSer}>
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="" required />
+            <input
+              onBlur={handleEmailBlur}
+              type="email"
+              name="email"
+              id=""
+              required
+            />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="" required />
+            <input
+              onBlur={handlePasswordBlur}
+              type="password"
+              name="password"
+              id=""
+              required
+            />
           </div>
           <div className="input-group">
             <label htmlFor="confirmpassword">Confirm Password</label>
-            <input type="password" name="confirmpassword" id="" required />
+            <input
+              onBlur={handleConfirmPasswordBlur}
+              type="password"
+              name="confirmpassword"
+              id=""
+              required
+            />
           </div>
+          <p style={{ color: "red" }}>{error}</p>
           <input className="form-submit" type="submit" value="Sign up" />
         </form>
         <p>
